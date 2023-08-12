88de5dc96f291a797963431f644e25759257b15a
myoaung99 <myomyintaung064@gmail.com>
2023-08-12 07:47:50 +0630

    connect entities to related modules and letting typeorm know about these at app.module entities

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "./user.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {
+  }
+}
